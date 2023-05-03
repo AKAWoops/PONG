23e77d3e1c84c5c557242b40c124de750c187c4a
Mark Ballinger <markballinger@optusnet.coml.au>
2023-05-03 20:59:53 +1000

    added collision detection
    added player inputs for keys pressed

--- PONG.cpp
+++ PONG.cpp
@@ -1,7 +1,11 @@
-// PONG.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #include <raylib.h>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <stdbool.h>
+#include <math.h>
+
+
 #define WIDTH 640 //width is 640px screen width
 #define HEIGHT 480 //height is 480px screen height
 
@@ -20,20 +24,24 @@ struct ball
 	float y;
 	float Ballx;
 	float Bally;
-	float j = 0;
+	float j = 10.0;
 
 
 };
 
-const char* intTochar(int a);
-bool chackCollision(int x1, float x2, int y1, float y2, int w1, int h1, float r);
+const char * intTochar(int a);
+bool checkCollision(int x1, float x2, int y1, float y2, int w1, int h1, float j);
 
 
 int main()
 {
 	struct player player;
-	player.x = 5;
-	player.y = 50;
+	player.x = 20;
+	player.y = 100;
+
+	struct player cpu;
+	cpu.x = WIDTH - 35;
+	cpu.y = 100;
 
 	struct ball ball;
 	ball.x = (WIDTH / 2) - ball.j;
@@ -41,38 +49,100 @@ int main()
 	ball.Ballx = -5;
 	ball.Bally = -5;
 
-	InitWindow(WIDTH, HEIGHT, "pong");
+
+
+	InitWindow(WIDTH, HEIGHT, "PONG CLONE");
 	SetTargetFPS(60);
 
 
 
+	do
+	{
 
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawRectangle(0, 0, (WIDTH / 2), HEIGHT, BLACK);
+		DrawRectangle((WIDTH / 2), 0, (WIDTH / 2), HEIGHT, WHITE);
+		DrawRectangle(player.x, player.y, player.width, player.height, WHITE);
+		DrawRectangle(cpu.x, cpu.y, cpu.width, cpu.height, BLACK);
+		DrawText(intTochar(player.score), (WIDTH / 2) - 200, 30, 48, WHITE);
+		DrawText(intTochar(cpu.score), (WIDTH / 2) + 200, 30, 48, BLACK);
 
-	BeginDrawing();
-	ClearBackground(BLACK);
-	DrawRectangle(0, 0, (WIDTH / 2), HEIGHT, BLACK);
-	DrawRectangle((WIDTH / 2), 0, (WIDTH / 2), HEIGHT, WHITE);
-	DrawRectangle(player.x, player.y, player.width, player.height, WHITE);
-	//DrawText(intTochar(player.score), (WIDTH / 2) - 200, 30, 48, WHITE);
+		if (checkCollision(player.x, ball.x, player.y, ball.y, player.width, player.height, ball.j) || checkCollision(cpu.x, ball.x, cpu.y, ball.y, cpu.width, cpu.height, ball.j))
+		{
+			ball.Ballx = -1 * ball.Ballx;	//if collides with ball and player is a player multiply to reverse the balls trajectory
+		}
 
-	//if (checkCollision(player.x, ball.x, player.y, ball.y, player.width, player.height, ball.j)
-	// {
-		//if collides with ball and player is a player multiply to reverse the balls trajectory
-	// }
-	
-		while (!WindowShouldClose());
+		cpu.y = ball.y;
+
+		if (ball.x < (WIDTH / 2)) DrawCircle(ball.x, ball.y, ball.j, WHITE);
+
+		else DrawCircle(ball.x, ball.y, ball.j, BLACK);
+		EndDrawing();
+
+		ball.x += ball.Ballx;
+		ball.y += ball.Bally;
+
+		if (ball.x < 5)
+		{
+			cpu.score++;
+			ball.x = (WIDTH / 2) - ball.j;
+			ball.y = 120;
+			ball.Ballx = -5;
+
+		}
+		else if (ball.x > WIDTH - 5)
+		{
+
+			player.score++;
+			ball.x = (WIDTH / 2) - ball.j;
+			ball.y = 120;
+			ball.Ballx = 5;
+		}
+
+		if (ball.y < 5)
+		{
+			ball.Bally = -1 * ball.Bally;
+
+		}
+		else if (ball.y > HEIGHT - 5)
+		{
+			ball.Bally = -(ball.Bally);
+		}// key inputs for player paddle
+		if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+		{
+
+			if (player.y < 5);
+			else player.y -= 5;
+		}
+		else if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+		{
+			if (player.y > (HEIGHT - player.height));
+			else player.y += 5;
+		}
+
+	} while (!WindowShouldClose());
 		
-		CloseWindow();
+	CloseWindow();
+
+	return 0;
+}
+
+const char* intTochar(int a) {
+	std::stringstream ss;
+	ss << a;
+	std::string str;
+	ss >> str;
+
+	return str.c_str();
+}
+
+bool checkCollision(int x1, float x2, int y1, float y2, int w1, int h1, float j) 
+{
+	if (x1 <= x2 + j && y1 <= y2 + j &&
+		x2 <= x1 + w1 && y2 <= y1 + h1)
+	{
+		return true;
 	}
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	else return false;
+}
